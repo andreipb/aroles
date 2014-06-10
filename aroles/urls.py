197c4ad1e93293dfad769df8d05b46a23ec30fb0
Samuel Dorsaz <samuel.dorsaz@students.hevs.ch>
2014-06-10 15:38:46 +0200

    Update for Django 1.6.5

--- aroles/urls.py
+++ aroles/urls.py
@@ -1,26 +1,18 @@
-from django.conf.urls.defaults import patterns, include
+from django.conf.urls import patterns, include, url
 from django.conf import settings
 #from django.contrib import admin
 #from django.conf.urls.static import static
-from django.conf.urls.defaults import url
 from views.acm_challenge_demo import get_segmented_videos
 
 urlpatterns = patterns('',
-url(r'^$', get_segmented_videos),
-url(r'/(?P<central_video_input_id>\d+)/seek/(?P<chosen_segment_id>\d+)', get_segmented_videos),
-url(r'/(?P<central_video_input_id>\d+)', get_segmented_videos),
-
- )
+	url(r'^$', get_segmented_videos),
+	url(r'/(?P<central_video_input_id>\d+)/seek/(?P<chosen_segment_id>\d+)', get_segmented_videos),
+	url(r'/(?P<central_video_input_id>\d+)', get_segmented_videos),
+)
 
 from dajaxice.core import dajaxice_autodiscover
 dajaxice_autodiscover()
 if 'dajaxice' in settings.INSTALLED_APPS:
-    urlpatterns += patterns('',
-        url(r'^%s/' % settings.DAJAXICE_MEDIA_PREFIX, include('dajaxice.urls')),
-    )
-
-
-
-
-
-
+	urlpatterns += patterns('',
+		url(r'^%s/' % settings.DAJAXICE_MEDIA_PREFIX, include('dajaxice.urls')),
+	)
